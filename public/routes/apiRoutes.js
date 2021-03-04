8e218bde00d3b014105ca2e8ff3a92cca44de59e
j-lopez2012381 <josu.lopez.barrios@gmail.com>
2021-03-04 15:30:18 -0600

    segundo COMMIT

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -4,19 +4,19 @@ const router = Router();
 //RUTAS DEL API
 router.get('/testMulti', (req, res) => {
     //res.render('index.ejs')
-    res.sendFile('/index.html', { 'root': '../API/views' });
+    res.sendFile('/index.html', { 'root': '../API_SO2/views' });
   
   });
   router.get('/style.css', function (req, res) {
-    res.sendFile('/style.css', { 'root': '../API/views' });
+    res.sendFile('/style.css', { 'root': '../API_SO2/views' });
   });
   
   router.get('/functions.js', function (req, res) {
-    res.sendFile('/functions.js', { 'root': '../API/views' });
+    res.sendFile('/functions.js', { 'root': '../API_SO2/views' });
   });
   
   router.get('/images/bkg.png', function (req, res) {
-    res.sendFile('/images/bkg.png', { 'root': '../API/views' });
+    res.sendFile('/images/bkg.png', { 'root': '../API_SO2/views' });
   });
   
   
